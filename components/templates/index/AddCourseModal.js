09e95923baaf9e98a61d498d6e6e27b359bfddd3
royamousavi <roya2002_rm@yahoo.com>
2023-12-25 22:28:32 +0200

    create addNewCourse

--- components/templates/index/AddCourseModal.js
+++ components/templates/index/AddCourseModal.js
@@ -14,12 +14,31 @@ import { useState } from "react";
 const AddCourseModal = ({ hideAddCourseModal }) => {
   const [title, setTitle] = useState("");
 
+  const addNewCourse = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch("/api/courses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title }),
+    });
+    if (res.status === 201) {
+      console.log("course create successfully ");
+      setTitle("");
+    }
+    const data = await res.json();
+  };
+
   return (
     <div className={styles.modal_container} id="add-new-course-modal">
       <div className={styles.modal_bg} onClick={hideAddCourseModal}></div>
       <div className={styles.modal_content}>
-        <h1 className={styles.modal_title}>اضافه کردن دوره جدید</h1>
-        <form action="#" className={styles.edit_user_form}>
+        <h1 className={styles.modal_title}>Add Course   </h1>
+        <form
+          action="#"
+          className={styles.edit_user_form}
+          onSubmit={addNewCourse}
+        >
           <div className={styles.input_field}>
             <span>
               <FontAwesomeIcon icon={faTag} />
@@ -28,12 +47,12 @@ const AddCourseModal = ({ hideAddCourseModal }) => {
               value={title}
               onChange={(event) => setTitle(event.target.value)}
               type="text"
-              placeholder="نام دوره"
-              spellcheck="false"
+              placeholder="Name course "
+              spellCheck="false"
             />
           </div>
           <button type="submit" className={styles.update_btn}>
-            اپدیت دوره
+             Create Course
           </button>
         </form>
       </div>
